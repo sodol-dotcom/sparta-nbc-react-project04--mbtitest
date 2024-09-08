1777e7a5e212b3fe80849847cb5c88f8c555acab
sodol-dotcom <sojeong0521@gmail.com>
2024-09-08 23:15:17 +0900

    feat: Add basic structures for Login, Signup, and Profile pages feat: 로그인, 회원가입, 프로필 페이지 기본 구조 추가

---
+++ src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import AuthForm from "../components/AuthForm";
+import { login, getUserProfile } from "../api/auth";
+import { Link, useNavigate } from "react-router-dom";
+
+const Login = ({ setUser }) => {
+
+
+  const handleLogin = async (formData) => {
+    try {
+
+    } catch (error) {
+      alert("로그인에 실패했습니다. 다시 시도해주세요.");
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>로그인</h1>
+        <AuthForm mode="login" onSubmit={handleLogin} />
+        <div>
+          <p>
+            계정이 없으신가요?{" "}
+            <Link to="/signup">
+              회원가입
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
