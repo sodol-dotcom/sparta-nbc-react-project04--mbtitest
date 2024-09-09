11b6b278980156f152effe0549b3ef79fe09941b
sodol-dotcom <sojeong0521@gmail.com>
2024-09-09 13:42:48 +0900

    fix: update Tailwind CSS configuration and import styles correctly 수정: Tailwind CSS 구성 및 스타일 올바르게 가져오기

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // src 폴더 내 모든 JavaScript, JSX, TypeScript 파일을 포함
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
